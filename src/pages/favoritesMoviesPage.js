649d656e641886e946a964c431965db63d08d07a
Michael Barcoe <barcoe98@gmail.com>
2020-11-06 13:07:45 +0000

    Fix buttons on movie list pages

--- src/pages/favoritesMoviesPage.js
+++ src/pages/favoritesMoviesPage.js
@@ -1,15 +1,15 @@
 import React from "react";
 import StubAPI from "../api/stubAPI";
 import PageTemplate from "../components/templateMovieListPage";
+import AddReviewButton from '../components/buttons/addReview'
 
 const FavoriteMoviesPage = props => {
-  const toDo = () => true;
 
   return (
     <PageTemplate
       movies={StubAPI.getAll()}
-      title={"Favourite Movies"}
-      buttonHandler={toDo}
+      title={"Favorite Movies"}
+      action={movie => <AddReviewButton movie={movie} />}
     />
   );
 };
