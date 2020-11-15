8ad6a2f39a853d036f85f9e1e909df0aa8220bcb
Michael Barcoe <barcoe98@gmail.com>
2020-11-15 19:20:10 +0000

    NavBar updated + Redesigned

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -8,25 +8,34 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 const SiteHeader = () => {
   return (
   <Navbar collapseOnSelect variant="dark" bg="" expand="lg" fixed="top">
+    <FontAwesomeIcon
+        className="navbar-text text-light"
+        icon={["fas", "film"]}
+        size="2x"
+      />
   <Navbar.Brand href="#home">TMDB</Navbar.Brand>
+  
+
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
+ 
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-    <Nav.Link className="nav-link text-white" href="/">Home</Nav.Link>
+    <Nav className="ml-auto">
+    <Nav.Link className="nav-link text-white" href="/">HOME</Nav.Link>
 
-      <NavDropdown className="text-white" title="Movies" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/movies/popular">Popular</NavDropdown.Item>
-        <NavDropdown.Item href="/movies/toprated">Top Rated</NavDropdown.Item>
-        <NavDropdown.Item href="/movies/upcoming">Upcoming</NavDropdown.Item>
+      <NavDropdown className="text-white" title="MOVIES" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/movies/popular">POPULAR</NavDropdown.Item>
+        <NavDropdown.Item href="/movies/toprated">TOP RATED</NavDropdown.Item>
+        <NavDropdown.Item href="/movies/upcoming">UPCOMING</NavDropdown.Item>
       </NavDropdown>
 
-      <NavDropdown title="Tv Shows" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/tvshows/upcoming">Popular</NavDropdown.Item>
-        <NavDropdown.Item href="/tvshows/toprated">Top Rated</NavDropdown.Item>
+      <NavDropdown title="TV SHOWS" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/tvshows/upcoming">POPULAR</NavDropdown.Item>
+        <NavDropdown.Item href="/tvshows/toprated">TOP RATED</NavDropdown.Item>
       </NavDropdown>
 
-      <Nav.Link href="/forum">Forum</Nav.Link>
+      <Nav.Link href="/forum">FORUM</Nav.Link>
+      <Nav.Link href="/forum">FAVOURITES</Nav.Link>
+      <Nav.Link href="/forum">WATCH LIST</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
