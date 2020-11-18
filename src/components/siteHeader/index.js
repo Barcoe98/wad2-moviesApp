747e837e07bac8f6c609c031e6e233dbfac5e8c5
Michael Barcoe <barcoe98@gmail.com>
2020-11-18 18:49:50 +0000

    WatchList/Favorites movies showing on page

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -1,36 +1,36 @@
 import React from "react";
 import "../../globals/fontawesome";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./siteHeader.css";
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import {  Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 const SiteHeader = () => {
   return (
-  <Navbar collapseOnSelect variant="dark" bg="" expand="lg" fixed="top">
+  <Navbar collapseOnSelect variant="dark" expand="lg" fixed="top">
     <FontAwesomeIcon
         className="navbar-text text-light"
         icon={["fas", "film"]}
         size="2x"
       />
-  <Navbar.Brand href="/home">TMDB</Navbar.Brand>
+  <Navbar.Brand href="/">TMDB</Navbar.Brand>
   
-
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
- 
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-    <Nav.Link className="nav-link text-white" href="/movies">HOME</Nav.Link>
 
-      <NavDropdown className="text-white" title="MOVIES" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/movies/popular">POPULAR</NavDropdown.Item>
-        <NavDropdown.Item href="/movies/toprated">TOP RATED</NavDropdown.Item>
-        <NavDropdown.Item href="/movies/upcoming">UPCOMING</NavDropdown.Item>
+      <Nav.Item><Link className="nav-link text-dark" to="/"> HOME</Link></Nav.Item>
+
+      <NavDropdown className="text-dark" title="MOVIES" id="basic-nav-dropdown">
+        <NavDropdown.Item><Link className="nav-link text-dark" to="/movies/popular"> POPULAR</Link></NavDropdown.Item>
+        <NavDropdown.Item><Link className="nav-link text-dark" to="/movies/toprated"> TOP RATED</Link></NavDropdown.Item>
+        <NavDropdown.Item><Link className="nav-link text-dark" to="/movies/upcoming"> UPCOMING</Link></NavDropdown.Item>
+        <NavDropdown.Item><Link className="nav-link text-dark" to="/movies/favorites"> FAVOURITES</Link></NavDropdown.Item>
+        <NavDropdown.Item><Link className="nav-link text-dark" to="/movies/watchlist"> WATCH LIST</Link></NavDropdown.Item>
       </NavDropdown>
 
-      <Nav.Link href="/forum">FORUM</Nav.Link>
-      <Nav.Link href="/movies/favorites">FAVOURITES</Nav.Link>
-      <Nav.Link href="/movies/watchlist">WATCH LIST</Nav.Link>
-    </Nav>
+    <Nav.Link className="nav-link text-dark" href="/forum">FORUM</Nav.Link>
+  </Nav>
   </Navbar.Collapse>
 </Navbar>
   )
