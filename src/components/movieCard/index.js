09024f4ecc2d572c180bd1054630a37f79e7da91
Michael Barcoe <barcoe98@gmail.com>
2020-12-11 16:27:58 +0000

    changed design on movie card

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -9,6 +9,7 @@ const MovieCard = ({movie, action}) => {
     <div className="col-sm-2">
       <div className="card">
       <div className="container">
+      
         <Link to={`/movies/${movie.id}`}>
       <img
       className="card-img-tag center"
@@ -20,13 +21,9 @@ const MovieCard = ({movie, action}) => {
       }
       />
       </Link>
-      
-      <div>
-      <h4 className="card-title">{movie.title}</h4>
-           {action(movie)}
-        </div>
+      <h4 className="card-title-m">{movie.title}</h4>
+      {action(movie)}
       </div>
-       
       </div>
     </div>
   );
